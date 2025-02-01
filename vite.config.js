3ca88db39d8812a42dda45435d75ebf5afd86475
Your Name <you@example.com>
2025-02-01 06:24:05 -0300

    add pipeline

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://pedrolucastp.github.io/mediquiz-vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
